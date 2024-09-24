f54a0f170f473a5905c635cf440fe7b6912683fd
karberlee <karber@foxmail.com>
2024-09-24 17:38:26 +0800

    update header and footer style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -82,6 +82,10 @@ header {
   justify-content: space-between;
   align-items: center;
   z-index: 999;
+  box-shadow: 
+    0 2px 4px -1px rgba(0, 0, 0, .2), 
+    0 4px 5px 0 rgba(0, 0, 0, .14), 
+    0 1px 10px 0 rgba(0, 0, 0, .12);
 }
 
 .main-button {
